4c510f4a0300553c0a9cfd77b9b1fd04eecf2ce6
张觉 <21373122@buaa.edu.cn>
2023-07-23 00:05:39 +0800

    增加两个查找函数

--- orm.py
+++ orm.py
@@ -24,14 +24,13 @@ class UserGroup(Model):
 
     class Meta:
         database = db
+        table_name = 'user_group'
 
 class Question(Model):
     '''记录一个问题'''
     id = AutoField(primary_key=True)
     creator_id = ForeignKeyField(User)
     content = TextField()
-    answer = TextField()
-    tags = CharField()
 
     class Meta:
         database = db
@@ -83,12 +82,12 @@ class ReQuestionToGroup(Model):
 class QuestionGroupPerm(Model):
     '''记录问题组的权限'''
     id = AutoField(primary_key=True)
-    user_id = ForeignKeyField(User)
-    group_id = ForeignKeyField(QuestionGroup)
+    user_group_id = ForeignKeyField(UserGroup)
+    question_group_id = ForeignKeyField(QuestionGroup)
     class Meta:
         database = db
         table_name = 'question_group_perm'
-        constraints = [SQL('UNIQUE(user_id, group_id)')]
+        constraints = [SQL('UNIQUE(user_group_id, question_group_id)')]
 
 class Manager:
     def try_login(self, username, pwhash):
@@ -96,6 +95,7 @@ class Manager:
         if len(users) == 0:
             return False
         return users[0].password == pwhash
+    
     def try_register(self, username, pwhash):
         users = User.select().where(User.username == username)
         if len(users) !=0:
@@ -103,6 +103,36 @@ class Manager:
         new_user = User(username = username, password = pwhash)
         new_user.save()
         return True
+    
+    def select_allowed_questiongroup_by_user(self, username):
+        '''
+        根据用户名，查看对用户可见的问题组
+        返回值为一个列表，列表中每个元素都有四个类变量，与QuestionGroup相同
+        '''
+        result = (QuestionGroup
+            .select(QuestionGroup.id, QuestionGroup.group_name, QuestionGroup.creator_id, QuestionGroup.is_public)
+            .join(QuestionGroupPerm, on=(QuestionGroupPerm.question_group_id == QuestionGroup.id))
+            .join(UserGroup, on=(QuestionGroupPerm.user_group_id == UserGroup.id))
+            .join(ReUserToGroup, on=(UserGroup.id == ReUserToGroup.group_id))
+            .join(User, on=(ReUserToGroup.user_id == User.id))
+            .where(User.username == username))
+        print(result[0].creator_id)
+        return result
+    
+    def select_allowed_questiongroup_by_usergroup(self, group_name):
+        '''
+        根据用户名，查看对某用户组可见的问题组
+        返回值为一个列表，列表中每个元素都有四个类变量，与QuestionGroup相同
+        '''
+        result = (QuestionGroup
+            .select(QuestionGroup.id, QuestionGroup.group_name, QuestionGroup.creator_id, QuestionGroup.is_public)
+            .join(QuestionGroupPerm, on=(QuestionGroupPerm.question_group_id == QuestionGroup.id))
+            .join(UserGroup, on=(QuestionGroupPerm.user_group_id == UserGroup.id))
+            .where(UserGroup.group_name == group_name))
+        print(result[0].group_name)
+        return result
+    
+    
 
 # User.create_table()
 # user = User(username = "lucas", password = "buaa2023")
@@ -110,4 +140,5 @@ class Manager:
 # all = User.get()
 # all.delete_instance()
 
-db.create_tables([UserGroup, QuestionGroup, SolutionHistory, ReUserToGroup, ReQuestionToGroup, QuestionGroupPerm])
+# db.create_tables([UserGroup, QuestionGroup, SolutionHistory, ReUserToGroup, ReQuestionToGroup, QuestionGroupPerm])
+Manager().select_allowed_questiongroup_by_usergroup('测试用户组')
